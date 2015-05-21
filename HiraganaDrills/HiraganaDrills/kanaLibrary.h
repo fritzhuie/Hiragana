92bbc7620d9aea9c6197f2a55b48a6ae272b17f2
Fritz <fritz@apportable.com>
2015-05-20 22:44:11 -0700

    fixed some library errors

--- HiraganaDrills/HiraganaDrills/kanaLibrary.h
+++ HiraganaDrills/HiraganaDrills/kanaLibrary.h
@@ -3,6 +3,7 @@
 extern NSDictionary* HIRAGANA_PAIR_DICT;
 extern NSDictionary* HIRAGANA_DICT;
 extern NSDictionary* KATAKANA_PAIR_DICT;
+extern NSDictionary* KATAKANA_DICT;
 
 
 void initializePairDict();
