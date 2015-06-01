7eed286a314baffcb47c844d36e7b944b9c6f6d6
Fritz <fritz@apportable.com>
2015-05-31 21:47:58 -0400

    bunch of kana word tapping stuff

--- HiraganaDrills/HiraganaDrills/FullWordViewController.m
+++ HiraganaDrills/HiraganaDrills/FullWordViewController.m
@@ -9,6 +9,10 @@
 #import "FullWordViewController.h"
 #import "kanaLibrary.h"
 
+#define WORD_VIEW_WIDTH 320.0f
+#define WORD_VIEW_HEIGHT 150.0f
+#define WORD_VIEW_Y 74.0f
+
 @interface FullWordViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *kanaWordLabel;
 @property (weak, nonatomic) IBOutlet UIView *wordView;
@@ -19,6 +23,7 @@
 @implementation FullWordViewController {
     NSArray* wordList;
     NSString* currentWord;
+    NSArray* views;
 }
 
 @synthesize wordView,tapGestureRecogniser, kanaWordLabel;
@@ -34,26 +39,29 @@
     kanaWordLabel.text = [currentWord stringByReplacingOccurrencesOfString:@"/" withString:@""];
 }
 
-- (NSString *)returnCharacterInTapBoundary:(CGPoint)location{
-    int wordLength = (int)(([currentWord length]/2)+1);
-    NSLog(@"%i", wordLength);
-    float boxWidth =  320.0 / wordLength;
-
+- (void)showAnswerForKanaAt:(CGPoint)location{
+    CGFloat wordLength = ([currentWord length]/2)+1;
+    NSLog(@"%fl", wordLength);
+    CGFloat boxWidth =  320.0 / wordLength;
+    //(WORD_VIEW_WIDTH, WORD_VIEW_HEIGHT); CGPointMake(0.0, WORD_VIEW_Y);
+    NSString* selectedKana;
     int i = 0;
     while(i < wordLength){
         if (location.x < ((i+1)*boxWidth)){
             NSArray* a = [currentWord componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"/"]];
-            //NSLog(@"Numer in array: %i | #%i | %@", wordLength, i, a[i]);
-            return a[i];
-        }
-        i++;
-    }
-    return nil;
+            selectedKana = a[i];
+            break;
+        }i++;}
+    CGRect tapBoxRect = CGRectMake(WORD_VIEW_WIDTH/wordLength*i, WORD_VIEW_Y, WORD_VIEW_WIDTH/wordLength, WORD_VIEW_HEIGHT);
+    UIView* answerView = [[UIView alloc]initWithFrame:tapBoxRect];
+    answerView.backgroundColor = [UIColor whiteColor];
+    answerView.opaque = YES;
+    //create opaque view over character with romanji, that animates fadeout
 }
 
 - (IBAction)tapDetected:(id)sender {
     NSLog(@"X:%f Y:%f", [tapGestureRecogniser locationInView:wordView].x, [tapGestureRecogniser locationInView:wordView].y);
-    NSString* a = [self returnCharacterInTapBoundary:[tapGestureRecogniser locationInView:wordView]];
+    [self showAnswerForKanaAt:[tapGestureRecogniser locationInView:wordView]];
 }
 
 - (void)didReceiveMemoryWarning {
